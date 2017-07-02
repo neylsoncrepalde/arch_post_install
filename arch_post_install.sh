fdc4174ea5a44299123c291a64a65e972c7c92af
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-07-01 22:14:22 -0300

    minor updates

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -1,13 +1,10 @@
-printf "Installing Firefox\n"
+printf "Installing Firefox\n\n"
 sudo pacman -S firefox
 
-printf "Installing Opera\n"
+printf "Installing Opera\n\n"
 sudo pacman -S opera
 
-printf "Installing Emacs\n"
-sudo pacman -S emacs
-
-printf "Installing yaourt\n"
+printf "Installing yaourt\n\n"
 sudo pacman -S --needed base-devel git wget yajl
 git clone https://aur.archlinux.org/package-query.git
 cd package-query
@@ -18,35 +15,43 @@ cd yaourt makepkg -si
 cd ..
 sudo rm -dR yaourt/ package-query/
 
-printf "Installing Musescore\n"
+printf "Installing Emacs and Sublime\n\n"
+sudo pacman -S emacs
+yaourt -S sublime-text-dev
+
+printf "Installing MS Fonts\n\n"
+yaourt -S ttf-ms-fonts
+
+printf "Installing Musescore\n\n"
 sudo pacman -S musescore
 
-printf "Installing R (last version available)\n"
+printf "Installing R (last version available)\n\n"
 sudo pacman -S r
 
-printf "Installing LaTeX\n"
+printf "Installing LaTeX\n\n"
 sudo pacman -S texlive-most texlive-langextra
 sudo pacman -S texstudio
 
-printf "Installing Java and Eclipse\n"
+printf "Installing Java and Eclipse\n\n"
 sudo pacman -S jdk8-openjdk
 sudo pacman -S eclipse-java
 
-printf "Making Java work in R\n"
+printf "Making Java work in R\n\n"
 sudo R CMD javareconf
 
-printf "Installing RStudio\n"
+printf "Installing RStudio\n\n"
 yaourt -S rstudio-desktop-bin
 
-printf "Installing Python Anaconda\n"
+printf "Installing Python Anaconda\n\n"
 cd Downloads
 wget http://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
 bash Anaconda3-4.4.0-Linux-x86_64.sh
 cd ..
 
-printf "Installing Spotify\n"
+printf "Installing Spotify\n\n"
 gpg --keyserver hkps://pgp.mit.edu --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 yaourt -S spotify
 
-printf "Installing some important libraries\n"
+printf "Installing some important libraries\n\n"
 sudo pacman -S ntfs-3g gcc-fortran
+
