a4049eb8100c46f6fc76d107e1af2e24150968e3
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-07-15 21:30:42 -0300

    echo changes

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -1,57 +1,58 @@
-printf "Installing Firefox\n\n"
+echo "Installing Firefox\n\n"
 sudo pacman -S firefox
 
-printf "Installing Opera\n\n"
+echo "Installing Opera\n\n"
 sudo pacman -S opera
 
-printf "Installing yaourt\n\n"
+echo "Installing yaourt\n\n"
 sudo pacman -S --needed base-devel git wget yajl
 git clone https://aur.archlinux.org/package-query.git
 cd package-query
 makepkg -si
 cd ..
 git clone https://aur.archlinux.org/yaourt.git
-cd yaourt makepkg -si
+cd yaourt 
+makepkg -si
 cd ..
 sudo rm -dR yaourt/ package-query/
 
-printf "Installing Emacs and Sublime\n\n"
+echo "Installing Emacs and Sublime\n\n"
 sudo pacman -S emacs
 yaourt -S sublime-text-dev
 
-printf "Installing MS Fonts\n\n"
+echo "Installing MS Fonts\n\n"
 yaourt -S ttf-ms-fonts
 
-printf "Installing Musescore\n\n"
+echo "Installing Musescore\n\n"
 sudo pacman -S musescore
 
-printf "Installing R (last version available)\n\n"
+echo "Installing R (last version available)\n\n"
 sudo pacman -S r
 
-printf "Installing LaTeX\n\n"
+echo "Installing LaTeX\n\n"
 sudo pacman -S texlive-most texlive-langextra
 sudo pacman -S texstudio
 
-printf "Installing Java and Eclipse\n\n"
+echo "Installing Java and Eclipse\n\n"
 sudo pacman -S jdk8-openjdk
 sudo pacman -S eclipse-java
 
-printf "Making Java work in R\n\n"
+echo "Making Java work in R\n\n"
 sudo R CMD javareconf
 
-printf "Installing RStudio\n\n"
+echo "Installing RStudio\n\n"
 yaourt -S rstudio-desktop-bin
 
-printf "Installing Python Anaconda\n\n"
+echo "Installing Python Anaconda\n\n"
 cd Downloads
 wget http://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
 bash Anaconda3-4.4.0-Linux-x86_64.sh
 cd ..
 
-printf "Installing Spotify\n\n"
+echo "Installing Spotify\n\n"
 gpg --keyserver hkps://pgp.mit.edu --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 yaourt -S spotify
 
-printf "Installing some important libraries\n\n"
+echo "Installing some important libraries\n\n"
 sudo pacman -S ntfs-3g gcc-fortran
 
